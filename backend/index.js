6c0cf0632aafc3b62f78be718bdfa05fd56cd3ea
Swathi <swathi.c2888@gmail.com>
2019-09-30 12:25:29 +0530

    12:25:2930/09/19

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 var express = require('express')
 var transaction = require('./transaction')
 var app = express()
-
+const uuidv1 = require('uuid/v1')
+var bodyParser = require('body-parser')
 const port = 3000
+app.use(bodyParser.json())
 
 app.listen(port, function(event) {
     console.log('Starting the app at http://localhost:' + port);
@@ -12,7 +14,8 @@ app.get('/', function(req, res) {
     res.send('You have reached the landing page. Please use only API calls to access the backend')
 })
 
-app.get('/transaction/add', function(req, res) {
-    let val = transaction.add()
-    res.send(val)
+app.post('/transaction/site', function(req, res) {
+    let params = req.body;
+    let response = transaction.site(params)
+    res.send(response)
 })
--- backend/transaction.js
+++ backend/transaction.js
@@ -1,10 +1,86 @@
+var sha1 = require('sha1')
+const uuidv1 = require('uuid/v1')
+
 var transaction = {
-    add: function() {
-        return 'Pinged Transaction.add function.'
+    site: function(params) {
+        return createSiteNumber(params)
     },
     view: function() {
 
     }
 }
+
+function createPatient(data) {
+
+}
+
+//Function to create a tansaction to add Site Number
+//Required parameters - name
+//Expected Output - siteId
+function createSiteNumber(params) {
+    let data = {}
+    data.tx = "site"
+    data.id = uuidv1()
+    data.name = params.name
+
+    let meta = addMeta(data)
+    let encapsulatedTx = {
+        data: data,
+        meta: meta
+    }
+    console.log(encapsulatedTx)
+    return encapsulatedTx
+}
+
+function createRegion(data) {
+
+}
+//Function to create Region ID for the site
+//Required parameters-Country Name
+//Expected output-Region Id.
+
+function createStrata(data) {
+
+}
+//Function to create Startification for the patient.
+//Required parameters-Age.
+//Expected output-Rand Id.
+
+function createRandNumber(data) {
+
+}
+//Function to assign Rand Number for the patient.
+//Required parameters-Age.
+//Expected output-Rand Id.
+
+/////////////////////////////////////////////////////////////////////////////////
+
+function createVisitType(data) {
+
+}
+
+
+//Function to create Visit Type  for the patient.
+//Required parameters-Scheduled/Unscheduled Visit
+//Expected output-Patient assign to visit type.
+
+////////////////////////////////////////////////////////////////////////////////////
+//function to create visit number for the patient.
+//Required parameters-Visit number to be enetered.
+//Expected output -Patient details updated with respective visit.
+function createVisitNumber(data) {
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+function addMeta(data) {
+    let meta = {
+        txHash: sha1(JSON.stringify(data)),
+        txTime: new Date().toISOString()
+    }
+    return meta
+}
+
+
 
 module.exports = transaction
